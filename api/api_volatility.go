cce96537e2748609e3b79885745e4c0ec95bbcd2
US <uriel.scott@protonmail.com>
2020-11-23 17:31:57 -0600

    Correct api call

--- api/api_volatility.go
+++ api/api_volatility.go
@@ -4,10 +4,9 @@ import (
 	"github.com/uscott/go-api-deribit/inout"
 )
 
-
 func (c *Client) GetHistoricalVolatility(currency string) (vols [][]float64, err error) {
 	arg := inout.HistVolIn{Ccy: currency}
 	result := inout.HistVolOut{}
-	err = c.Call("get_historical_volatility",&arg,&result)
+	err = c.Call("public/get_historical_volatility", &arg, &result)
 	return result.Result, err
 }
