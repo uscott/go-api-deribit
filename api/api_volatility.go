c6b304eed66f921ba90fc95ab9b7f0fab74c6d4d
US <uriel.scott@protonmail.com>
2020-11-23 17:55:03 -0600

    Add comment to GetHistoricalVolatility

--- api/api_volatility.go
+++ api/api_volatility.go
@@ -4,6 +4,9 @@ import (
 	"github.com/uscott/go-api-deribit/inout"
 )
 
+// GetHistoricalVolatility returns slice of []float64 where each []float64 is of length 2
+// with the first element being the exchange time stamp and the 2nd the historical vol in
+// percent points as measured at the time stamp
 func (c *Client) GetHistoricalVolatility(currency string) (vols [][]float64, err error) {
 	arg := inout.HistVolIn{Ccy: currency}
 	err = c.Call("public/get_historical_volatility", &arg, &vols)
